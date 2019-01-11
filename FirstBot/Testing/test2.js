ee161e439b82964f4c2d209993d6722e22d4741b
RyanSharafuddin <rzs_0000@hotmail.com>
2019-01-11 03:31:10 -0500

    more utilities

--- FirstBot/Testing/test2.js
+++ FirstBot/Testing/test2.js
@@ -4,23 +4,15 @@ import * as utilities from '../utilities.js'
 import * as navigation from '../navigation.js'
 import * as robotFunctions from '../robotFunctions.js'
 
-var map1 = [
-  [1, 1, 3],
-  [1, 1, 3],
-  [3, 3, 1]
-];
-
-var map2 = [
-  [3, 3, 3],
-  [3, 3, 3],
-  [4, 4, 4,]
-];
-
-var map3 = [
-  [1, 1, 3],
-  [1, 1, 3],
-  [6, 6, 6]
-];
-
-var maps = [map1, map2, map3];
-console.log(navigation.compareColumn(maps, 0, 1));
+// var buildableOffsets = [[1, -1], [-1, 1], [1, 0], [-1,-1]];
+// var a = {}
+// robotFunctions.rememberStartingConnectedComponents(a, buildableOffsets);
+// console.log(JSON.stringify(a.startingConnectedComponents));
+// var Loc1 = [0, 2];
+// var Loc2 = [0, 2];
+// var Loc3 = new Location(2, 0);
+//
+// var set = new Set([Loc1]);
+// console.log(set.has(Loc1));
+// console.log(set.has(Loc2));
+// console.log(set.has(Loc3));
